a5f4eb134531e2c777a5b0ae70ecd6c15d7edceb
rukmal <rukmal@ddilabs.ai>
2021-06-24 09:58:05 +0530

    initial

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [
+    {
+      name : 'Gateway',
+      scripts : 'gatway/server.js',
+      watch : true,
+      env : {
+        
+      }
+    }
+  ],
+
+  // deploy : {
+  //   production : {
+  //     user : 'SSH_USERNAME',
+  //     host : 'SSH_HOSTMACHINE',
+  //     ref  : 'origin/master',
+  //     repo : 'GIT_REPOSITORY',
+  //     path : 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': ''
+  //   }
+  // }
+};
